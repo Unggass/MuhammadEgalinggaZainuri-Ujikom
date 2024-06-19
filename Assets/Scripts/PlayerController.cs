5a0779fc7cec69bd6d1c3b967d6747428e469c1b
Unggass <egalingga371@gmail.com>
2024-06-19 09:40:13 +0700

    update : Organize prefabs and Character movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,17 +5,25 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public GameObject foodPrefab;
+    private Rigidbody rb;
+
+    public float moveSpeed = 5.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+        float horizontalInput = Input.GetAxis("Horizontal");
+        Vector3 move = new Vector3 (horizontalInput, 0, 0) * Time.deltaTime * moveSpeed;
+
+        transform.Translate(move);
+
+        if(Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Mouse0))
         {
             Instantiate(foodPrefab, transform.position, foodPrefab.transform.rotation);
         }
