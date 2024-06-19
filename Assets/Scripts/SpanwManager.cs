1986438b8f5208e3e0934ed725cba6fb4957009e
Unggass <egalingga371@gmail.com>
2024-06-19 10:12:58 +0700

    add ; spawn Manager script for spawning random animal on random position

---
+++ Assets/Scripts/SpanwManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpanwManager : MonoBehaviour
+{
+    public GameObject[] animalPrefabs;
+
+    private float spawnRange = 10;
+    private float spawnPosZ = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnAnimal", 2f, 1.5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            SpawnAnimal();
+        }
+    }
+
+    void SpawnAnimal()
+    {
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+
+        Vector3 spawnPosition = new Vector3(Random.Range(-spawnRange, spawnRange), 0, spawnPosZ);
+        Instantiate(animalPrefabs[animalIndex], spawnPosition, animalPrefabs[animalIndex].transform.rotation);
+    }
+}
